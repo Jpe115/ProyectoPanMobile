17898e1c5743fa384d39c2ce000d6ee9482e0b31
Josue Palafox <jpalafoxe90@gmail.com>
2023-05-24 00:57:56 -0700

    Probablemente codigo terminado para poder usar la lista de productos

---
+++ ProyectoPanMobile/ViewModels/CarritoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ProyectoPanMobile.Data;
+using ProyectoPanMobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPanMobile.ViewModels
+{
+    public partial class CarritoViewModel : ObservableObject
+    {
+        public ObservableCollection<PanesConFoto> panesconfotoList { get; set; }
+        public List<PanesConFoto> panesLista { get; set; }
+
+        public CarritoViewModel(List<PanesConFoto> panesLizta)
+        {
+            panesconfotoList = new ObservableCollection<PanesConFoto>();
+            panesLista = panesLizta;
+        }
+
+        public async Task CargarPanes()
+        {
+            await Task.Run(() =>
+            {
+                panesconfotoList.Clear();
+                foreach (var panecitos in panesLista)
+                {
+                    panesconfotoList.Add(panecitos);
+                }
+            });
+            
+        }
+    }
+}
--- ProyectoPanMobile/Views/Carrito.xaml.cs
+++ ProyectoPanMobile/Views/Carrito.xaml.cs
@@ -1,9 +1,15 @@
+using ProyectoPanMobile.Models;
+using ProyectoPanMobile.ViewModels;
+
 namespace ProyectoPanMobile.Views;
 
 public partial class Carrito : ContentPage
 {
-	public Carrito()
+    public PanesConFoto Panes { get; set; }
+
+    public Carrito(List<PanesConFoto> panesLista)
 	{
 		InitializeComponent();
-	}
+		this.BindingContext = new CarritoViewModel(panesLista);
+    }
 }
